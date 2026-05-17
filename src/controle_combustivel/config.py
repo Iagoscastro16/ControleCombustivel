90c7ec3586dd31db459103d9003d84e450399fff
iagoscastro16 <iagoscastro16@gmail.com>
2026-05-16 22:32:04 -0300

    feat: tabelas do sistema e criação do config.py

---
+++ src/controle_combustivel/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+ROOT = Path(__file__).parent.parent.parent
+DB_PATH = ROOT/ "data" / "combustivel.db"
+DATA_DIR = ROOT / "data"
+
---
+++ src/controle_combustivel/database.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import sqlite3
+from config import  DB_PATH
+
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn 
+
+def criar_tabelas():
+    try:
+        with get_connection() as conn:
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS veiculos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT NOT NULL,
+                    categoria TEXT NOT NULL
+                    )
+            ''')
+    except Exception as e:
+        return {"success": False,
+                "message": f"Erro na criação da tabela 'veiculos': {e}"
+                }
+    try:        
+        with get_connection() as conn:
+            conn.execute('''
+                CREATE TABLE IF NOT EXISTS abastecimentos(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    data TEXT NOT NULL,
+                    veiculo_id INTEGER NOT NULL,
+                    valor REAL NOT NULL,
+                    FOREIGN KEY (veiculo_id) REFERENCES veiculos(id)
+                )
+            ''')
+    except Exception as e:
+        return {"success": False,
+                "message": f"Erro na criação da tabela 'abastecimentos': {e}"}
+    
+    return ("success": True, "message":"Tabelas criadas com sucesso")
